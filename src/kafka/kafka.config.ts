8f00a80a4713f4e6795a6ba2c792a1b4580ff640
RZ-Aniket <aniket.sharma@rezolut.in>
2025-06-25 07:24:40 +0530

    docker implemented

--- src/kafka/kafka.config.ts
+++ src/kafka/kafka.config.ts
@@ -2,5 +2,6 @@ import { Kafka } from 'kafkajs';
 
 export const kafka = new Kafka({
   clientId: 'chat-app',
-  brokers: ['localhost:9092'], 
+  // brokers: ['localhost:9092'], 
+   brokers: ['kafka:9092']
 });
